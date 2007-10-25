3fca8597109aaf6d9688cb1fedd30322814b69a2
Gordon <gordon@example.com>
2007-10-25 18:23:56 +0000

    cleanup
    git-svn-id: http://svn.symfony-project.com/plugins/sfGuardDoctrinePlugin/trunk@5686 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/model/doctrine/PluginsfGuardUserTable.class.php
+++ lib/model/doctrine/PluginsfGuardUserTable.class.php
@@ -5,15 +5,13 @@
  */
 class PluginsfGuardUserTable extends Doctrine_Table
 {
-	  # should this be a static function?
-    static public function retrieveByUsername( $username, $isActive = true )
-    {
-      $query = new Doctrine_Query();
-      return $query->from('sfGuardUser u')->where( 'u.username = ? AND u.is_active = ?', array( $username, $isActive ) )->execute()->getFirst();
-    }
+  static public function retrieveByUsername( $username, $isActive = true )
+  {
+    return Doctrine_Query::create()->from('sfGuardUser u')->where( 'u.username = ? AND u.is_active = ?', array( $username, $isActive ) )->execute()->getFirst();
+  }
 
-		static function retrieveByUsernameOrEmailAddress($usernameOrEmail)
-		{
-		  throw new Exception('Override this function in your sfGuardUserTable class so it queries for the user based on the username or email address');
-		}
+  static function retrieveByUsernameOrEmailAddress($usernameOrEmail)
+  {
+    throw new Exception('Override this function in your sfGuardUserTable class so it queries for the user based on the username or email address');
+  }
 }
