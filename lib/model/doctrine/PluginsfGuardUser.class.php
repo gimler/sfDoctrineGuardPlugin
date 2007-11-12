678be7d6e2656c13e2bbbac25e00aa3852760721
Gordon <gordon@example.com>
2007-11-12 11:14:50 +0000

    [wiki:sfGuardDoctrinePlugin] generate salt only on insert user
    git-svn-id: http://svn.symfony-project.com/plugins/sfGuardDoctrinePlugin/trunk@5984 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/model/doctrine/PluginsfGuardUser.class.php
+++ lib/model/doctrine/PluginsfGuardUser.class.php
@@ -23,8 +23,11 @@ abstract class PluginsfGuardUser extends BasesfGuardUser
         return $this->get('password');
       }
 
-      $salt = md5( rand( 100000, 999999 ) . $this->get('username') );
-      $this->set('salt', $salt );
+      if ( ! $salt = $this->get('salt'))
+      {
+        $salt = md5( rand( 100000, 999999 ) . $this->get('username') );
+        $this->set('salt', $salt );
+      }
       $algorithm = sfConfig::get( 'app_sf_guard_plugin_algorithm_callable', 'sha1' );
       $algorithmAsStr = is_array( $algorithm ) ? $algorithm[ 0 ].'::' . $algorithm[ 1 ] : $algorithm;
       
