9a81ccce91538dc173c2f98779546ac3811da66a
Gordon <gordon@example.com>
2007-12-06 10:11:13 +0000

    [sfGuardDoctrinePlugin] correct labels
    git-svn-id: http://svn.symfony-project.com/plugins/sfGuardDoctrinePlugin/branches/symfony-1.0@6335 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- modules/sfGuardRegister/templates/_form.php
+++ modules/sfGuardRegister/templates/_form.php
@@ -7,25 +7,25 @@
     <fieldset>
       <div class="form-row" id="sf_guard_register_username">
         <?php echo form_error('user[username]') ?>
-        <label for="username"><?php echo __('username'); ?>:</label>
+        <label for="user_username"><?php echo __('username'); ?>:</label>
         <?php echo input_tag('user[username]', $sf_data->get('sf_params')->get('user[username]')) ?>
       </div>
       
       <div class="form-row" id="sf_guard_register_email_address">
         <?php echo form_error('user[email_address]') ?>
-        <label for="username"><?php echo __('email address'); ?>:</label>
+        <label for="user_email_address"><?php echo __('email address'); ?>:</label>
         <?php echo input_tag('user[email_address]', $sf_data->get('sf_params')->get('user[email_address]')) ?>
       </div>
       
       <div class="form-row" id="sf_guard_register_password">
         <?php echo form_error('user[password]') ?>
-        <label for="password"><?php echo __('password'); ?>:</label>
+        <label for="user_password"><?php echo __('password'); ?>:</label>
         <?php echo input_password_tag('user[password]', $sf_data->get('sf_params')->get('user[password]')) ?>
       </div>
       
       <div class="form-row" id="sf_guard_register_password_confirmation">
         <?php echo form_error('user[password_confirmation]') ?>
-        <label for="password_confirmation"><?php echo __('password confirmation'); ?>:</label>
+        <label for="user_password_confirmation"><?php echo __('password confirmation'); ?>:</label>
         <?php echo input_password_tag('user[password_confirmation]', $sf_data->get('sf_params')->get('user[password_confirmation]')) ?>
       </div>
     </fieldset>
