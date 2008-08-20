d5de9c34b1315ea942c0b74a03415d2aba27b195
Gordon Franke <gordon.franke@web.de>
2008-08-20 08:58:58 +0000

    fix #4188 thanks to bschussek
    git-svn-id: http://svn.symfony-project.com/plugins/sfDoctrineGuardPlugin/trunk@10994 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- modules/sfGuardAuth/templates/signinSuccess.php
+++ modules/sfGuardAuth/templates/signinSuccess.php
@@ -1,4 +1,4 @@
-<form action="<?php echo url_for('@sf_guard_signin') ?>" method="post" />
+<form action="<?php echo url_for('@sf_guard_signin') ?>" method="post">
   <table>
     <?php echo $form ?>
   </table>
