f70b554a2e438545f5d6753a0aa3c051d1959123
Gordon Franke <gordon.franke@web.de>
2008-09-02 19:37:53 +0000

    sfDoctrineGuardPlugin add translation to signin template
    git-svn-id: http://svn.symfony-project.com/plugins/sfDoctrineGuardPlugin/trunk@11295 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- modules/sfGuardAuth/templates/signinSuccess.php
+++ modules/sfGuardAuth/templates/signinSuccess.php
@@ -1,8 +1,10 @@
+<?php use_helper('I18N') ?>
+
 <form action="<?php echo url_for('@sf_guard_signin') ?>" method="post">
   <table>
     <?php echo $form ?>
   </table>
 
-  <input type="submit" value="sign in" />
-  <a href="<?php echo url_for('@sf_guard_password') ?>">Forgot your password?</a>
+  <input type="submit" value="<?php echo __('sign in') ?>" />
+  <a href="<?php echo url_for('@sf_guard_password') ?>"><?php echo __('Forgot your password?') ?></a>
 </form>
