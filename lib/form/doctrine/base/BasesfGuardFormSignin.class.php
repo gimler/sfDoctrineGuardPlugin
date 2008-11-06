406dac70a3823aca8bd0af7bbaef17fe17a9a8bf
Jonathan Wage <jonathan.wage@symfony-project.org>
2008-11-06 15:49:11 +0000

    [1.1, 1.2] sfDoctrineGuardPlugin: fixes #4742
    git-svn-id: http://svn.symfony-project.com/plugins/sfDoctrineGuardPlugin/trunk@12685 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/form/doctrine/base/BasesfGuardFormSignin.class.php
+++ lib/form/doctrine/base/BasesfGuardFormSignin.class.php
@@ -7,11 +7,13 @@ class BasesfGuardFormSignin extends sfForm
     $this->setWidgets(array(
       'username' => new sfWidgetFormInput(),
       'password' => new sfWidgetFormInput(array('type' => 'password')),
+      'remember' => new sfWidgetFormInputCheckbox(),
     ));
 
     $this->setValidators(array(
       'username' => new sfValidatorString(),
       'password' => new sfValidatorString(),
+      'remember' => new sfValidatorBoolean(),
     ));
 
     $this->validatorSchema->setPostValidator(new sfGuardValidatorUser());
