e23b9a4e6e4e30c67e26c6728643c7f8edde2def
Romain Dorgueil <romain.dorgueil@symfony-project.com>
2009-02-25 15:36:02 +0000

    [sfDoctrineGuardPlugin][1.0][1.1][1.2][trunk] fixed hasCredential() method to admit an anonymous user to have an empty credential list
    git-svn-id: http://svn.symfony-project.com/plugins/sfDoctrineGuardPlugin/branches/symfony-1.0@15776 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/user/sfGuardSecurityUser.class.php
+++ lib/user/sfGuardSecurityUser.class.php
@@ -21,6 +21,11 @@ class sfGuardSecurityUser extends sfBasicSecurityUser
 
     public function hasCredential($credential, $useAnd = true)
     {
+        if ( empty($credential) )
+        {
+            return true;
+        }
+
         if ( !$this->getGuardUser() )
         {
             return false;
